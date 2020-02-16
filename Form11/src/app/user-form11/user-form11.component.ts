251d75d124524e78a2ab1af992ce6b775902f6e6
Gulati <sg185387@ncr.com>
2020-02-16 13:13:45 +0530

    Samples for user and HR created

---
+++ Form11/src/app/user-form11/user-form11.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form11',
+  templateUrl: './user-form11.component.html',
+  styleUrls: ['./user-form11.component.css']
+})
+export class UserForm11Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
