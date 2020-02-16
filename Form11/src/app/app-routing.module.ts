9e673cce527263e8fdb157a702d1fbfefafed965
Gulati <sg185387@ncr.com>
2020-02-16 14:34:31 +0530

    Routing module added

--- Form11/src/app/app-routing.module.ts
+++ Form11/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { UserForm11Component } from '../app/user-form11/user-form11.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:UserForm11Component},
+  {path: 'pf-pdf',component: ContainerComponent},
+  {path: 'pf',component:UserForm11Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
